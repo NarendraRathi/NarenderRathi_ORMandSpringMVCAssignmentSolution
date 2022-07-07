11883692b3fb70fe932b5bee8b5a6cf37e75b9cf
NarendraRathi <brathi255@gmail.com>
2022-07-07 20:31:35 +0530

    Customer Relationship Management
    commiting changes

---
+++ CustomerRelationshipManagement/src/main/java/com/greatlearning/javafsd/customerrelationshipmanagement/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.greatlearning.javafsd.customerrelationshipmanagement.service;
+
+import java.util.List;
+
+import com.greatlearning.javafsd.customerrelationshipmanagement.entity.Customer;
+
+
+public interface CustomerService {
+
+	List<Customer> listAll();
+
+	Customer findById(int theId);
+
+	void save(Customer thecustomer);
+
+	void deleteById(int theId);
+
+}
